daf8b9248326c6304bbf1ab5bef531148b09e197
ZtRXR <Zengtudor@outlook.com>
2023-08-02 18:55:30 +0800

    配置好了跨域和post请求解析

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
 
 const app = express()
 
-app.listen(81,()=>{
-    console.log("is running")
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const settings = {
+    port: 8080,
+    listenIp: '0.0.0.0',
+    crossDomainWhitelist: [
+        'http://localhost:4000'
+    ]
+}
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (settings.crossDomainWhitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    
+    res.send("hello")
 })
+
+
+app.listen(settings.port, () => {
+    console.log("服务器已经启动在" + settings.listenIp + ":" + settings.port)
+})
+
